69eaf6d2dd4af76da5ff0bd77ef51f9831e40591
Florian Hanzer <florian.hanzer@uibk.ac.at>
2020-05-13 22:07:09 +0200

    Add function for calculating sky view factor

--- openamundsen/terrain.py
+++ openamundsen/terrain.py
@@ -1,4 +1,5 @@
 import numpy as np
+import openamundsen.modules.radiation
 
 
 def slope_aspect(dem, res):
@@ -110,3 +111,75 @@ def normal_vector(dem, res):
     normal_vec /= normal_vec_len  # make it a unit vector
 
     return normal_vec
+
+
+def sky_view_factor(dem, res, azim_step=10, elev_step=1, logger=None):
+    """
+    Calculate the sky view factor for a DEM after Corripio (2003).
+    The sky view factor is the hemispherical fraction of unobstructed sky
+    visible from any point. Calculation is performed using a hillshading
+    algorithm at regular azimuth and elevation angle intervals.
+
+    Parameters
+    ----------
+    dem : ndarray
+        Terrain elevation (m).
+
+    res : float
+        DEM resolution (m).
+
+    azim_step : int, default 10
+        Azimuth angle interval.
+
+    elev_step : int, default 1
+        Elevation angle interval.
+
+    logger : Logger, optional
+        Logger instance for printing status messages.
+
+    Returns
+    -------
+    svf : ndarray
+        Sky view factor (values between 0 and 1).
+
+    References
+    ----------
+    .. [1] Corripio, J. G. (2003). Vectorial algebra algorithms for calculating
+       terrain parameters from DEMs and solar radiation modelling in mountainous
+       terrain. International Journal of Geographical Information Science, 17(1),
+       1–23. https://doi.org/10.1080/13658810210157796
+    """
+    slope, _ = slope_aspect(dem, res)
+
+    min_azim_angle = 0
+    max_azim_angle = 360
+    azim_angles = np.arange(min_azim_angle, max_azim_angle, azim_step)
+
+    min_elev_angle = 1
+    max_elev_angle = int(np.ceil(np.nanmax(slope)))
+    elev_angles = np.arange(min_elev_angle, max_elev_angle)[::-1]
+
+    svf = np.zeros(dem.shape)
+
+    for azim_angle in azim_angles:
+        if logger is not None:
+            logger.debug(f'Calculating sky view factor: azimuth={azim_angle}')
+
+        azim_rad = np.deg2rad(azim_angle)
+        svf_cur = np.full(dem.shape, max_elev_angle)
+
+        for elev_angle in elev_angles:
+            elev_rad = np.deg2rad(elev_angle)
+            sun_vec = np.array([
+                np.sin(azim_rad) * np.cos(elev_rad),
+                -np.cos(azim_rad) * np.cos(elev_rad),
+                np.sin(elev_rad),
+            ])
+
+            illum = openamundsen.modules.radiation.shadows(dem, res, sun_vec)
+            svf_cur[illum == 1] = elev_angle
+
+        svf += np.cos(np.deg2rad(svf_cur))**2
+
+    svf /= len(azim_angles)
+    return svf
