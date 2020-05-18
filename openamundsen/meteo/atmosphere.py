Fix dew point temperature calculation
always use vapor pressure over water

openamundsen/meteo/atmosphere.py
@@ -316,14 +316,17 @@ def wet_bulb_temperature(temp, rel_hum, vap_press, psych_const):
     return xk
 
 
-def dew_point_temperature(vap_press):
+def dew_point_temperature(temp, rel_hum):
     """
     Calculate dew point temperature.
 
     Parameters
     ----------
-    vap_press : numeric
-        Vapor pressure (Pa).
+    temp : numeric
+        Air temperature (K).
+
+    rel_hum : numeric
+        Relative humidity (%).
 
     Returns
     -------
@@ -331,8 +334,8 @@ def dew_point_temperature(vap_press):
         Dew point temperature (K).
     """
     ca, cb, cc = VAPOR_PRESSURE_COEFFS_WATER
-    vap_press = np.asarray(vap_press)
-    td_c = cc * np.log(vap_press / ca) / (cb - np.log(vap_press / ca))
+    vap_press_water = vapor_pressure(temp, rel_hum, 'water')
+    td_c = cc * np.log(vap_press_water / ca) / (cb - np.log(vap_press_water / ca))
     return td_c + c.T0
 
 

tests/test_meteo.py
@@ -91,13 +91,13 @@ def test_relative_humidity():
 
 
 def test_dew_point_temperature():
-    temps = np.array([15, 20, 25, 30, 35]) + 273.15
-    rel_hums = np.array([50, 70, 90, 100, 30])
-    tds = np.array([4.65, 14.36, 23.24, 30, 14.84]) + 273.15
-    vapor_pressures = meteo.vapor_pressure(temps, rel_hums)
+    temps = np.array([-5, 5, 15, 20, 25, 30, 35]) + 273.15
+    rel_hums = np.array([30, 50, 50, 70, 90, 100, 30])
+    tds = np.array([-19.8, -4.6, 4.65, 14.36, 23.24, 30, 14.84]) + 273.15
 
     assert_allclose(
-        meteo.dew_point_temperature(vapor_pressures),
+        meteo.dew_point_temperature(temps, rel_hums),
         tds,
-        atol=5e-2,
+        atol=0.1,
+    )
     )