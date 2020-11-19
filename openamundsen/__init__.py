f307e518c1acc3c0f55513ef785640fa046d6e8b
Florian Hanzer <florian.hanzer@uibk.ac.at>
2020-11-19 10:44:44 +0100

    Set __all__ in __init__.py

--- openamundsen/__init__.py
+++ openamundsen/__init__.py
@@ -1,4 +1,12 @@
-from .conf import Configuration, read_config
+from .conf import Configuration, parse_config, read_config
 from .model import OpenAmundsen, Model
 from . import constants, errors, terrain
 from ._version import __version__
+
+
+__all__ = [
+    'OpenAmundsen',
+    'Configuration',
+    'parse_config',
+    'read_config',
+]
