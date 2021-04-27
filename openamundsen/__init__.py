8cd0335b1a01a980ac761f57c26f6ffdc2806977
Florian Hanzer <florian.hanzer@uibk.ac.at>
2021-04-27 12:04:51 +0200

    Check for Python version

--- openamundsen/__init__.py
+++ openamundsen/__init__.py
@@ -1,3 +1,10 @@
+# Check for Python version
+import sys
+if sys.version_info < (3, 6):
+    sys.exit('openAMUNDSEN requires Python 3.6+.')
+del sys
+
+
 from .conf import Configuration, parse_config, read_config
 from .model import OpenAmundsen, Model
 from . import constants, errors, terrain
