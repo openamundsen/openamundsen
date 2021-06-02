74bb3910ef5c76c55acac30c5f3024c8421351ce
Florian Hanzer <florian.hanzer@uibk.ac.at>
2021-06-02 10:52:19 +0200

    Use setuptools_scm for handling the package version

--- openamundsen/__init__.py
+++ openamundsen/__init__.py
@@ -2,18 +2,29 @@ from .conf import Configuration, parse_config, read_config
 from .model import OpenAmundsen, Model
 from . import constants, errors, terrain
 
-# from pkg_resources import get_distribution, DistributionNotFound
-# try:
-#     __version__ = get_distribution(__name__).version
-# except DistributionNotFound:
-#     __version__ = '0.0.0'
-# del get_distribution, DistributionNotFound
 
-from importlib.metadata import version, PackageNotFoundError
-try:
-    __version__ = version("package-name")
-except PackageNotFoundError:
-    __version__ = '0.0.0'
+# Get version (method as used by matplotlib: https://github.com/matplotlib/matplotlib/blob/bcc1ce8461f5b6e874baaaa02ef776d0243a4abe/lib/matplotlib/__init__.py#L133-L151)
+def __getattr__(name):
+    if name == '__version__':
+        from pathlib import Path
+        import setuptools_scm
+        global __version__
+        root = Path(__file__).resolve().parents[1]
+        if (root / '.git').exists() and not (root / '.git/shallow').exists():
+            __version__ = setuptools_scm.get_version(
+                root=root,
+                version_scheme='post-release',
+                fallback_version='0.0.0+UNKNOWN',
+            )
+        else:
+            try:
+                from . import _version
+                __version__ = _version.version
+            except ImportError:
+                __version__ = '0.0.0+UNKNOWN'
+        return __version__
+    raise AttributeError(f'module {__name__!r} has no attribute {name!r}')
+
 
 __all__ = [
     'OpenAmundsen',
