83876196de7bd4fa6edbb1032d75e8713d6d09ef
Florian Hanzer <florian.hanzer@uibk.ac.at>
2021-06-02 09:58:54 +0200

    Move build setup to setup.cfg

--- openamundsen/__init__.py
+++ openamundsen/__init__.py
@@ -1,15 +1,19 @@
-# Check for Python version
-import sys
-if sys.version_info < (3, 6):
-    sys.exit('openAMUNDSEN requires Python 3.6+.')
-del sys
-
-
 from .conf import Configuration, parse_config, read_config
 from .model import OpenAmundsen, Model
 from . import constants, errors, terrain
-from ._version import __version__
 
+# from pkg_resources import get_distribution, DistributionNotFound
+# try:
+#     __version__ = get_distribution(__name__).version
+# except DistributionNotFound:
+#     __version__ = '0.0.0'
+# del get_distribution, DistributionNotFound
+
+from importlib.metadata import version, PackageNotFoundError
+try:
+    __version__ = version("package-name")
+except PackageNotFoundError:
+    __version__ = '0.0.0'
 
 __all__ = [
     'OpenAmundsen',
