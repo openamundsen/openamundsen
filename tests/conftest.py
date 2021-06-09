c07ca478e2b446ee72a61ae1ffc30f3457287964
Florian Hanzer <florian.hanzer@uibk.ac.at>
2021-06-09 16:59:38 +0200

    Add snow tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,30 @@
 import openamundsen as oa
+import copy
 import pooch
+import pytest
+import textwrap
+
+version = oa.__version__
+data_fetcher = pooch.create(
+    path=pooch.os_cache('openamundsen-tests'),
+    base_url='https://github.com/openamundsen/testdata/raw/{version}/',
+    version=version,
+    version_dev='main',
+    registry={  # hashes generated with generate_hashes.py
+        'grids/rofental/dem_rofental_1000.asc': '368ee374bfbb927bae5408a38dbafbbeab4ff59dc94ae30956b617bf28c114ec',
+        'grids/rofental/roi_rofental_1000.asc': '5c495833e8465931d7aedc982ef093332572034d9cf7da5fc8b98b72111e3d25',
+        'grids/rofental/srf_rofental_1000.asc': '431f0ed2181abc2a79c6ffd79b804e5c983c9c47003bce57b853db2b41abf62d',
+        'grids/rofental/svf_rofental_1000.asc': 'a4b7704fad6f7c63445a76c64207ff881b73023ec7d958d4581cd7fb42d06966',
+        'meteo/rofental/csv/bellavista.csv': 'fab913929aa84e04a5c47c9c4ea89ed613b8ecc14cf54194a090600de4ce9d98',
+        'meteo/rofental/csv/latschbloder.csv': '2d6a8a6297464d36ff9ccfc34ce4cd44a50f398c35de1a6e32e8922871503ff0',
+        'meteo/rofental/csv/proviantdepot.csv': '7a068c921127c5e0f15ed3718c3a257c70146111ae8a1c5e97f0f8db1e60d2bc',
+        'meteo/rofental/csv/stations.csv': '175b420d9659d3478d4755fc4b8d8c2569400b37d84ffb9906e1ab661afe5dc4',
+        'meteo/rofental/netcdf/bellavista.nc': '0ae9350ec9b092724775496a0c449f27d825cb30e7059ede206ca26032a0fd6a',
+        'meteo/rofental/netcdf/latschbloder.nc': '0a885ebd2b70818a4e6c30498346827e61cd2e983a311eb9fc5b3257f5b01bd2',
+        'meteo/rofental/netcdf/proviantdepot.nc': 'ecb70cdd0bbd1d87d77fcc0bebb3f28212b2aeae687380cf3a67c1b954f29c91',
+    },
+)
+DATA_DIR = data_fetcher.abspath
 
 
 def pytest_sessionstart(session):
@@ -7,26 +32,57 @@ def pytest_sessionstart(session):
 
 
 def fetch_data_files():
-    version = oa.__version__
-    data_fetcher = pooch.create(
-        path=pooch.os_cache('openamundsen-tests'),
-        base_url='https://github.com/openamundsen/testdata/raw/{version}/',
-        version=version,
-        version_dev='main',
-        registry={  # hashes generated with generate_hashes.py
-            'grids/rofental/dem_rofental_1000.asc': '368ee374bfbb927bae5408a38dbafbbeab4ff59dc94ae30956b617bf28c114ec',
-            'grids/rofental/roi_rofental_1000.asc': '5c495833e8465931d7aedc982ef093332572034d9cf7da5fc8b98b72111e3d25',
-            'grids/rofental/srf_rofental_1000.asc': '431f0ed2181abc2a79c6ffd79b804e5c983c9c47003bce57b853db2b41abf62d',
-            'grids/rofental/svf_rofental_1000.asc': 'a4b7704fad6f7c63445a76c64207ff881b73023ec7d958d4581cd7fb42d06966',
-            'meteo/rofental/csv/bellavista.csv': 'fab913929aa84e04a5c47c9c4ea89ed613b8ecc14cf54194a090600de4ce9d98',
-            'meteo/rofental/csv/latschbloder.csv': '2d6a8a6297464d36ff9ccfc34ce4cd44a50f398c35de1a6e32e8922871503ff0',
-            'meteo/rofental/csv/proviantdepot.csv': '7a068c921127c5e0f15ed3718c3a257c70146111ae8a1c5e97f0f8db1e60d2bc',
-            'meteo/rofental/csv/stations.csv': '175b420d9659d3478d4755fc4b8d8c2569400b37d84ffb9906e1ab661afe5dc4',
-            'meteo/rofental/netcdf/bellavista.nc': '0ae9350ec9b092724775496a0c449f27d825cb30e7059ede206ca26032a0fd6a',
-            'meteo/rofental/netcdf/latschbloder.nc': '0a885ebd2b70818a4e6c30498346827e61cd2e983a311eb9fc5b3257f5b01bd2',
-            'meteo/rofental/netcdf/proviantdepot.nc': 'ecb70cdd0bbd1d87d77fcc0bebb3f28212b2aeae687380cf3a67c1b954f29c91',
-        },
-    )
-
     for file in data_fetcher.registry_files:
         data_fetcher.fetch(file)
+
+
+@pytest.fixture(scope='session')
+def base_config():
+    config_yaml = textwrap.dedent(f'''
+        domain: rofental
+        start_date: 2020-01-15
+        end_date: 2020-04-30
+        resolution: 1000
+        timestep: 3H
+        crs: "epsg:32632"
+        timezone: 1
+
+        input_data:
+          grids:
+            dir: {DATA_DIR}/grids/rofental
+          meteo:
+            dir: {DATA_DIR}/meteo/rofental/netcdf
+
+        output_data:
+          timeseries:
+            format: memory
+
+            variables:
+              - var: snow.num_layers
+                name: num_snow_layers
+              - var: snow.albedo
+                name: snow_albedo
+    ''')
+
+    config = oa.Configuration.from_yaml(config_yaml)
+    full_config = oa.parse_config(config)
+
+    return full_config
+
+
+@pytest.fixture(scope='session')
+def base_config_point_results(base_config):
+    model = oa.OpenAmundsen(base_config)
+    model.initialize()
+    model.run()
+    return model.point_outputs.data
+
+
+@pytest.fixture(scope='function')
+def base_config_single_point_results(base_config_point_results):
+    return base_config_point_results.sel(point='proviantdepot')
+
+
+@pytest.fixture(scope='function')
+def single_point_results_multilayer(base_config_single_point_results):
+    return base_config_single_point_results
---
+++ tests/test_snow.py
@@ -0,0 +1,162 @@
+import numpy as np
+from numpy.testing import assert_allclose, assert_equal
+import openamundsen as oa
+import pytest
+import warnings
+
+
+single_point_results_all = [
+    pytest.lazy_fixture('single_point_results_multilayer'),
+    pytest.lazy_fixture('single_point_results_cryolayers'),
+]
+
+
+@pytest.fixture(scope='module')
+def point_results_cryolayers(base_config):
+    config = base_config.copy()
+    config.snow.model = 'cryolayers'
+    config.output_data.timeseries.variables.append({'var': 'snow.cold_content'})
+    config.output_data.timeseries.variables.append({'var': 'snow.layer_albedo'})
+    model = oa.OpenAmundsen(config)
+    model.initialize()
+    model.run()
+    return model.point_outputs.data
+
+
+@pytest.fixture(scope='function')
+def single_point_results_multilayer(base_config_single_point_results):
+    return base_config_single_point_results
+
+
+@pytest.fixture(scope='function')
+def single_point_results_cryolayers(point_results_cryolayers):
+    return point_results_cryolayers.sel(point='proviantdepot')
+
+
+def test_default_multilayer(base_config):
+    assert base_config.snow.model == 'multilayer'
+
+
+@pytest.mark.parametrize('ds', single_point_results_all)
+def test_swe(ds):
+    swe = ds.swe.values
+    ice_content = ds.ice_content.values
+    liquid_water_content = ds.liquid_water_content.values
+    assert np.all(np.isfinite(swe))
+    assert swe.min() == 0.
+    assert_allclose(
+        swe,
+        (ice_content + liquid_water_content).sum(axis=1),
+        atol=0.1,
+    )
+    assert np.all(ice_content >= 0.)
+    assert np.all(liquid_water_content >= 0.)
+
+
+def test_swe_max_mean(single_point_results_multilayer, single_point_results_cryolayers):
+    swe = single_point_results_multilayer.swe.values
+    assert_allclose(swe.max(), 123.95, atol=1)
+    assert_allclose(swe.mean(), 67.38, atol=1)
+
+    swe = single_point_results_cryolayers.swe.values
+    assert_allclose(swe.max(), 109.6, atol=1)
+    assert_allclose(swe.mean(), 56.29, atol=1)
+
+
+@pytest.mark.parametrize('ds', single_point_results_all)
+def test_density(ds):
+    swe3d = (ds.ice_content + ds.liquid_water_content).values
+    density = ds.snow_density.values
+    assert np.all(np.isnan(density[swe3d == 0.]))
+    assert np.all(np.isfinite(density[swe3d > 0.]))
+    assert not np.any(density < 0.)
+    assert not np.any(density > 1000.)
+
+
+@pytest.mark.parametrize('ds', single_point_results_all)
+def test_depth(ds):
+    depth = ds.snow_depth.values
+    swe = ds.swe.values
+    assert np.all(depth[swe == 0.] == 0.)
+    assert np.all(depth[swe > 0.] > 0.)
+    assert not np.any(depth < 0.)
+
+    swe3d = (ds.ice_content + ds.liquid_water_content).values
+    thickness = ds.snow_thickness.values
+    assert_allclose(depth, thickness.sum(axis=1), atol=1e-3)
+
+    with warnings.catch_warnings():
+        warnings.filterwarnings(
+            'ignore',
+            'invalid value encountered in true_divide',
+            RuntimeWarning,
+        )
+        density_calc = swe3d / thickness
+
+    density = ds.snow_density.values
+    assert_allclose(density_calc, density, atol=1.)
+
+
+@pytest.mark.parametrize('ds', single_point_results_all)
+def test_num_layers(ds):
+    num_layers = ds.num_snow_layers.values
+    thickness = ds.snow_thickness.values
+    assert_equal(num_layers, (thickness > 0).sum(axis=1))
+
+
+@pytest.mark.parametrize('ds', single_point_results_all)
+def test_albedo(ds, base_config):
+    albedo = ds.snow_albedo.values
+    swe = ds.swe.values
+    min_albedo = base_config.snow.albedo.min
+    max_albedo = base_config.snow.albedo.max
+    pos_snow = swe > 0.
+    assert np.all(albedo[pos_snow] >= min_albedo)
+    assert np.all(albedo[pos_snow] <= max_albedo)
+    assert np.all(np.isnan(albedo[~pos_snow]))
+
+
+@pytest.mark.parametrize('ds', single_point_results_all)
+def test_melt(ds):
+    assert np.all(ds.melt >= 0)
+
+
+@pytest.mark.parametrize('ds', single_point_results_all)
+def test_runoff(ds):
+    assert np.all(ds.runoff >= 0)
+
+
+def test_cryolayers_layer_albedo(single_point_results_cryolayers, base_config):
+    ds = single_point_results_cryolayers
+    layer_albedo = ds.layer_albedo.values
+    swe3d = (ds.ice_content + ds.liquid_water_content).values
+    min_albedo = base_config.snow.albedo.min
+    max_albedo = base_config.snow.albedo.max
+    pos_snow = swe3d > 0.
+    assert np.all(layer_albedo[pos_snow] >= min_albedo)
+    assert np.all(layer_albedo[pos_snow] <= max_albedo)
+    assert np.all(np.isnan(layer_albedo[~pos_snow]))
+
+
+def test_cryolayers_cold_content(single_point_results_cryolayers, base_config):
+    ds = single_point_results_cryolayers
+    cc = ds.cold_content.values
+    ic = ds.ice_content.values
+    lwc = ds.liquid_water_content.values
+    melt = ds.melt.values
+    sublimation = ds.sublimation.values
+    swe3d = (ds.ice_content + ds.liquid_water_content).values
+    cold_holding_capacity = base_config.snow.cryolayers.cold_holding_capacity
+    pos_snow = swe3d > 0
+    assert np.all(cc >= 0.)
+    assert cc.max() > 0.
+    assert np.all(cc[~pos_snow] == 0.)
+
+    # Check if the CC is <= swe * cold_colding_capacity (add melt and sublimation because the
+    # maximum CC is calculated before reducing SWE by melt and sublimation)
+    assert np.all(
+        (
+            ((ic + lwc).sum(axis=1) + melt + sublimation) * cold_holding_capacity
+            - cc.sum(axis=1)
+        ) >= -0.1
+    )
