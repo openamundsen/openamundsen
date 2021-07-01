2dbc5f297362c0fa2c35bc0e018aeca15886537b
Florian Hanzer <florian.hanzer@uibk.ac.at>
2021-07-01 14:40:44 +0200

    Add numba/numpy tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -27,6 +27,25 @@ data_fetcher = pooch.create(
 DATA_DIR = data_fetcher.abspath
 
 
+def pytest_addoption(parser):
+    parser.addoption('--run-slow', action='store_true', default=False, help='run slow tests')
+
+
+def pytest_configure(config):
+    config.addinivalue_line('markers', 'slow: mark test as slow to run')
+
+
+def pytest_collection_modifyitems(config, items):
+    if config.getoption('--run-slow'):
+        return
+
+    skip_slow = pytest.mark.skip(reason='need --run-slow option to run')
+
+    for item in items:
+        if 'slow' in item.keywords:
+            item.add_marker(skip_slow)
+
+
 def pytest_sessionstart(session):
     fetch_data_files()
 
---
+++ tests/test_numerical.py
@@ -0,0 +1,66 @@
+from openamundsen.util import to_yaml
+import os
+import pytest
+import subprocess
+import textwrap
+import xarray as xr
+
+
+def test_numba(base_config, tmp_path):
+    config = base_config.copy()
+    config.start_date = '2020-01-18'
+    config.end_date = '2020-01-18'
+    config.results_dir = tmp_path
+    config.output_data.timeseries.format = 'netcdf'
+    config.output_data.grids.format = 'netcdf'
+    config.output_data.grids.variables = [
+        {'var': 'meteo.temp'},
+        {'var': 'snow.swe'},
+        {'var': 'snow.temp', 'name': 'snow_temp'},
+    ]
+
+    config_file = tmp_path / 'config.yml'
+    with open(config_file, 'w') as f:
+        f.write(to_yaml(config.toDict()))
+
+    subprocess.check_call(['openamundsen', config_file])
+    ds_points_numba = xr.load_dataset(tmp_path / 'output_timeseries.nc')
+    ds_grids_numba = xr.load_dataset(tmp_path / 'output_grids.nc')
+
+    env = os.environ.copy()
+    env['NUMBA_DISABLE_JIT'] = '1'
+    subprocess.check_call(['openamundsen', config_file], env=env)
+    ds_points_nonumba = xr.load_dataset(tmp_path / 'output_timeseries.nc')
+    ds_grids_nonumba = xr.load_dataset(tmp_path / 'output_grids.nc')
+
+    assert ds_points_numba.equals(ds_points_nonumba)
+    assert ds_grids_numba.equals(ds_grids_nonumba)
+
+
+@pytest.mark.slow
+def test_floating_errors(base_config, tmp_path):
+    config = base_config.copy()
+    config.results_dir = tmp_path
+
+    config_file = tmp_path / 'config.yml'
+    with open(config_file, 'w') as f:
+        f.write(to_yaml(config.toDict()))
+
+    with open(tmp_path / 'run.py', 'w') as f:
+        f.write(textwrap.dedent(
+            f'''
+            import numpy as np
+            np.seterr(all='raise')
+
+            import openamundsen as oa
+
+            config = oa.read_config('{config_file}')
+            model = oa.OpenAmundsen(config)
+            model.initialize()
+            model.run()
+            '''
+        ))
+
+    env = os.environ.copy()
+    env['NUMBA_DISABLE_JIT'] = '1'
+    subprocess.check_call(['python', tmp_path / 'run.py'], env=env)
