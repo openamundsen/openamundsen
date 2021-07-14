5953fc09b63a39e1cbf2c7c065d32484880fb0d3
Florian Hanzer <florian.hanzer@uibk.ac.at>
2021-07-14 11:17:29 +0200

    Use console_scripts entry point for the openamundsen command line tool (#53)

---
+++ tests/test_cli.py
@@ -0,0 +1,15 @@
+from openamundsen.util import to_yaml
+import subprocess
+
+
+def test_cli(base_config, tmp_path):
+    config = base_config.copy()
+    config.start_date = '2020-01-18 00:00'
+    config.end_date = '2020-01-18 00:00'
+    config.results_dir = tmp_path
+
+    config_file = tmp_path / 'config.yml'
+    with open(config_file, 'w') as f:
+        f.write(to_yaml(config.toDict()))
+
+    subprocess.check_call(['openamundsen', config_file])
