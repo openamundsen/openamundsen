21e98464ebed4ca36d7bd2cbfb5fab56675a7ef9
Florian Hanzer <florian.hanzer@uibk.ac.at>
2021-09-28 13:50:21 +0200

    Simplify creation of point forcing datasets (#67)

--- openamundsen/fileio/meteo.py
+++ openamundsen/fileio/meteo.py
@@ -121,7 +121,7 @@ def read_meteo_data(
     if len(datasets) == 0:
         raise errors.MeteoDataError('No meteo data available for the specified period')
 
-    ds_combined = _combine_meteo_datasets(datasets)
+    ds_combined = combine_point_datasets(datasets)
     dates_combined = ds_combined.time.to_index()
     if dates_combined[0] > start_date or dates_combined[-1] < end_date:
         raise errors.MeteoDataError('Insufficient meteo data available.\n'
@@ -159,52 +159,7 @@ def read_netcdf_meteo_file(filename):
     rename_dict = {v: constants.NETCDF_VAR_MAPPINGS[v] for v in rename_vars}
     ds = ds.rename_vars(rename_dict)
 
-    for var in ('time', 'lon', 'lat', 'alt'):
-        if var not in ds:
-            raise oa.errors.MeteoDataError(f'File is missing "{var}" variable: {filename}')
-
-    # Special case for precipitation - convert rates to sums first if required
-    if 'precip' in ds and (
-        'units' not in ds['precip'].attrs  # if units are not specified we assume they are kg m-2 s-1
-        or ds['precip'].units == 'kg m-2 s-1'
-    ):
-        try:
-            freq = pd.infer_freq(ds.indexes['time'])
-            dt = util.offset_to_timedelta(freq).total_seconds()
-        except ValueError:  # infer_freq() needs at least 3 dates
-            dt = np.nan
-
-        ds['precip'] *= dt
-        ds['precip'].attrs['units'] = 'kg m-2'
-
-    for var, meta in constants.METEO_VAR_METADATA.items():
-        units = meta['units']
-
-        if var in ds and 'units' in ds[var].attrs and ds[var].units != units:
-            raise oa.errors.MeteoDataError(
-                f'{var} has wrong units in {filename}: expected {units}, got {ds[var].units}'
-            )
-
-    # remove all unsupported variables
-    ds_vars = list(ds.variables.keys())
-    allowed_vars = ['time', 'lon', 'lat', 'alt'] + list(constants.NETCDF_VAR_MAPPINGS.values())
-    drop_vars = list(set(ds_vars) - set(allowed_vars))
-    ds = ds.drop_vars(drop_vars)
-
-    station_id = _netcdf_station_id(filename)
-
-    # set station name to station id (filename without extension) if not present
-    if 'station_name' in ds.attrs:
-        station_name = ds.attrs['station_name']
-    else:
-        station_name = station_id
-
-    # remove all attributes except id and name
-    ds.attrs = {
-        'station_id': station_id,
-        'station_name': station_name,
-    }
-
+    ds = make_point_dataset(data=ds, point_id=_netcdf_station_id(filename))
     return ds
 
 
@@ -260,26 +215,21 @@ def read_csv_meteo_file(filename, station_id, station_name, x, y, alt, crs):
         pass
         # TODO check if the time step is really 3600 s and if precip is really a sum
 
-    datadict = {param: (['time'], df[param].astype(np.float32)) for param in df.columns}
-    datadict.update(
-        time=(['time'], df.index),
-        lon=([], lon),
-        lat=([], lat),
-        alt=([], alt),
+    ds = make_point_dataset(
+        data=df,
+        point_id=station_id,
+        name=station_name,
+        lon=lon,
+        lat=lat,
+        alt=alt,
     )
-    ds = xr.Dataset(datadict)
-
-    ds.attrs = {
-        'station_id': station_id,
-        'station_name': station_name,
-    }
 
     return ds
 
 
-def _combine_meteo_datasets(datasets):
+def combine_point_datasets(datasets):
     """
-    Combine a list of meteo datasets as read by read_netcdf_meteo_file.
+    Combine a list of meteo datasets as returned by `make_point_dataset`.
     The datasets are merged by adding an additional "station" (= station id)
     dimension.
     """
@@ -586,3 +536,179 @@ def _apply_station_rules(meta, bounds, exclude, include):
 
     meta = meta[~meta.index.duplicated(keep='first')]  # remove potential duplicate entries
     return meta
+
+
+def make_point_dataset(
+        data=None,
+        dates=None,
+        point_id=None,
+        name=None,
+        lon=None,
+        lat=None,
+        alt=None,
+):
+    """
+    Create a dataset containing the meteorological forcing data for a single
+    point including metadata (point ID and name, variable units).
+
+    Parameters
+    ----------
+    data : xr.Dataset or pd.DataFrame, optional
+        Point data. If None, a dataset containing all-nan values will be
+        created.
+
+    dates : pd.DatetimeIndex, optional
+        Must be specified only if `data` is None, otherwise the dates will be
+        taken from there.
+
+    point_id : str, optional
+        Point ID. Only required if the ID is not stored in `data.attrs`.
+
+    name : str, optional
+        Point name. If `None`, the name is taken from `data.attrs` if possible,
+        otherwise the name will be set to the point ID.
+
+    lon : float, optional
+        Longitude (degrees). Must be specified only if `data` is None or does
+        not contain a `lon` variable.
+
+    lat : float, optional
+        Latitude (degrees). Must be specified only if `data` is None or does
+        not contain a `lat` variable.
+
+    alt : float, optional
+        Altitude (m). Must be specified only if `data` is None or does
+        not contain an `alt` variable.
+
+    Returns
+    -------
+    ds : xr.Dataset
+    """
+    if data is None:
+        data = xr.Dataset()
+
+        if dates is None:
+            dates = pd.DatetimeIndex([])
+    elif isinstance(data, xr.Dataset):
+        data = data.copy(deep=True)  # required because of the potential precipitation-altering
+        dates = data.indexes['time']
+    elif isinstance(data, pd.DataFrame):
+        data = data.copy()
+        data.index.name = 'time'
+        dates = data.index
+        data = data.to_xarray()
+    else:
+        raise TypeError(f'Unsupported type: {type(data)}')
+
+    if point_id is None:
+        if data is None or 'station_id' not in data.attrs:
+            raise TypeError('Point ID neither in data nor passed as keyword argument')
+        point_id = data.attrs['station_id']
+
+    if name is None:
+        try:
+            name = data.attrs['station_name']
+        except KeyError:
+            name = point_id
+
+    if lon is None:
+        if 'lon' not in data.variables:
+            raise TypeError('Longitude neither in data nor passed as keyword argument')
+        lon = float(data.lon)
+
+    if lat is None:
+        if 'lat' not in data.variables:
+            raise TypeError('Latitude neither in data nor passed as keyword argument')
+        lat = float(data.lat)
+
+    if alt is None:
+        if 'alt' not in data.variables:
+            raise TypeError('Altitude neither in data nor passed as keyword argument')
+        alt = float(data.alt)
+
+    ds = make_empty_point_dataset(dates, point_id, name, lon, lat, alt)
+
+    # Copy values (and check if the units are as expected)
+    for var, meta in constants.METEO_VAR_METADATA.items():
+        if var not in data:
+            continue
+
+        if var == 'precip':
+            # Convert precipitation rates to sums first if required
+            if 'units' in data['precip'].attrs and data['precip'].units == 'kg m-2 s-1':
+                try:
+                    freq = pd.infer_freq(dates)
+                    dt = util.offset_to_timedelta(freq).total_seconds()
+                except ValueError:  # infer_freq() needs at least 3 dates
+                    dt = np.nan
+
+                data['precip'] *= dt
+                data['precip'].attrs['units'] = 'kg m-2'
+
+        units = meta['units']
+        if 'units' in data[var].attrs and data[var].units != units:
+            raise oa.errors.MeteoDataError(
+                f'{var} has wrong units: expected {units}, got {ds[var].units}'
+            )
+
+        ds[var].values[:] = data[var].values
+
+    return ds
+
+
+def make_empty_point_dataset(dates, point_id, name, lon, lat, alt):
+    """
+    Create an empty dataset for the meteorological forcing data for a single
+    point.
+
+    The returned dataset contains the station metadata (point ID, name,
+    coordinates and altitude) and the meteorological variables, which are set to
+    all-nan values.
+
+    Parameters
+    ----------
+    dates : pd.DatetimeIndex
+        Dates.
+
+    point_id : str
+        Point ID.
+
+    name : str
+        Point name
+
+    lon : float
+        Longitude (degrees).
+
+    lat : float
+        Latitude (degrees).
+
+    alt : float
+        Altitude (m)
+
+    Returns
+    -------
+    ds : xr.Dataset
+    """
+    data = {
+        'lon': ([], float(lon)),
+        'lat': ([], float(lat)),
+        'alt': ([], float(alt)),
+    }
+
+    for var, meta in constants.METEO_VAR_METADATA.items():
+        data[var] = (
+            ['time'],
+            np.full(len(dates), np.nan),
+            meta,
+        )
+
+    ds = xr.Dataset(
+        data,
+        coords={'time': (['time'], dates)},
+        attrs={
+            'station_id': point_id,
+            'station_name': name,
+        },
+    )
+
+    return ds
