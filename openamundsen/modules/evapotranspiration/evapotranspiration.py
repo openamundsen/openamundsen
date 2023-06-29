Fix bug in evapotranspiration calculation for water bodies
The previous definition of pos_snow resulted in evapotranspiration for
all non-water pixels being set to 0 (which is not a problem if water is
class 1 as in the default land cover class definitions, since the other
classes are processed afterwards).

openamundsen/modules/evapotranspiration/evapotranspiration.py
@@ -284,7 +284,7 @@ class EvapotranspirationModel:
             # Ignore snow cover for water
             if is_water_body:
                 pos_snowfree = model.global_mask(pos[roi])
-                pos_snow = model.global_mask(~pos[roi])
+                pos_snow = np.full(pos_snowfree.shape, False)
 
             # Calculate crop ET under standard conditions
             if crop_coefficient_type == 'single':