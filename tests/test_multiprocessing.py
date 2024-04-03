dd72ba685b46c1d648edea6a3ed6abd6613426a9
Florian Hanzer <florian.hanzer@gmail.com>
2024-04-03 17:42:52 +0200

    Add multiprocessing test

---
+++ tests/test_multiprocessing.py
@@ -0,0 +1,25 @@
+from .conftest import base_config
+import multiprocessing
+import openamundsen as oa
+
+
+def run_model(model):
+    model.run()
+
+
+def test_multiprocessing():
+    config = base_config()
+    config.end_date = '2020-01-16'
+    config.output_data.timeseries.format = 'memory'
+    config.output_data.grids.format = 'memory'
+
+    config.snow.model = 'multilayer'
+    model1 = oa.OpenAmundsen(config)
+    model1.initialize()
+
+    config.snow.model = 'cryolayers'
+    model2 = oa.OpenAmundsen(config)
+    model2.initialize()
+
+    with multiprocessing.Pool() as pool:
+        pool.map(run_model, [model1, model2])
